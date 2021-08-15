3656eeb1cdfdefac6d1dd0f820f616d57dd61c19
demonicirfan <hashtag.irfan@gmail.com>
2021-08-15 11:13:24 +0530

    was not able to connect ot database previousely now i changed the password its working if you get the same error use the password conrrectly recheck or reset your password

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routers/authRoutes");
 
 const app = express();
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // database connection
-const dbURI = "mongodb+srv://Irfan:Premium123@cluster0.finly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+const dbURI =
+  "mongodb+srv://irfan:f6acBEmu2qkbohd6@cluster0.onw2w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
 // routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.use(authRoutes);
 
-
-//Premium123
+//f6acBEmu2qkbohd6

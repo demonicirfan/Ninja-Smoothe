2377e39b6179aa4886db293f12aa9f479ba8017a
Irfan Asif <hashtag.irfan@gmail.com>
2021-10-12 00:58:56 +0530

    🛸

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -32,17 +33,19 @@ const checkUser = (req, res, next) => {
       'this is a secret do not expose it',
       async (err, decodedToken) => {
         if (err) {
-          console.log(err.message);
+          res.locals.user = null;
           next();
         } else {
-          console.log(decodedToken);
-          let user = await user.findById(decodedToken.id);
+          let user = await User.findById(decodedToken.id);
+          res.locals.user = user;
           next();
         }
       }
     );
   } else {
+    res.locals.user = null;
+    next();
   }
 };
 
-module.exports = { requireAuth };
+module.exports = { requireAuth, checkUser };
